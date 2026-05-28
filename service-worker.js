c6ba133dc4df404585b816cdbe71fcde75e6f682
fabrineri <fabrizio.neri@gmail.com>
2026-05-28 12:23:36 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'spesa-v1';
+const ASSETS = [
+  'spesa.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
